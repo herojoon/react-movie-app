821f63747086ea28fe338da500ea5ac6d9b3f2be
herojoon <herojoon432@gmail.com>
2020-11-22 06:23:20 +0900

    add movie-app code remove package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+import './App.css';
 
 class App extends React.Component {
     state = {
         isLoading: true,
+        movies: []
     };
 
     componentDidMount() {
         // 영화 데이터 로딩!
-        setTimeout(() => {
-           this.setState({isLoading: false})
-        }, 6000);
+        this.getMovies();
+    }
+
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies }  // axiosGetData.data.data.movies
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        //this.setState({movies: movies});
+        this.setState({movies, isLoading: false});  // {movies:movies}  state와 대입할 변수의 이름이 같으면 {movies}로 축약된다.
     }
 
     render() {
-        const {isLoading} = this.state;
+        const {isLoading, movies} = this.state;
         return (
-            <div>
-                {isLoading ? 'Loading...' : 'We are ready?'}
-            </div>
+            <section className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <span className="loader__text">Loading...</span>
+                    </div>
+                ) : (
+                <div className="movies">
+                    {movies.map(movie => (
+                    <Movie
+                        key={movie.id}
+                        id={movie.id}
+                        year={movie.year}
+                        title={movie.title}
+                        summary={movie.summary}
+                        poster={movie.medium_cover_image}
+                        genres={movie.genres}
+                    />
+                ))}
+                </div>
+            )}
+            </section>
         );
     }
 }
